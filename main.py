5a165a8ebc0f09c4d6495ad01096642f0927c8b9
EC2 Default User <ec2-user@ip-172-31-33-221.eu-north-1.compute.internal>
2026-02-05 07:00:56 +0000

    First CI/CD deploy

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello from FastAPI!"}
+    return {"message": "Hello Mammad! from FastAPI!"}
 
 @app.get("/health")
 def health():
